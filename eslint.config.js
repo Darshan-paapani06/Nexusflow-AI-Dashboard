5c3c7299b94c4568f3cf65701496b2f87f570ca9
Darshan Gowda T S <darshanpaapani@gmail.com>
2026-08-03 22:19:44 +0530

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import globals from "globals";
+import globals from "globals"; 
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
